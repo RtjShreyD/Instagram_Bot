2b50ddb14cb24df5b7955e0ff2cd6e89b2cf06d3
RtjShreyD <shreyanshece1041@gmail.com>
2020-04-27 18:41:26 +0530

    Liking by Tags

--- instapy_bot.py
+++ instapy_bot.py
@@ -1,5 +1,10 @@
 from instapy import InstaPy
 
-session = InstaPy(username="<username>", password="<password>")
+session = InstaPy(username="<username>", password="<password>", headless_browser=True)
 
 session.login()
+session.set_quota_supervisor(enabled=True, peak_comments_daily=240, peak_comments_hourly=21)
+session.set_relationship_bounds(enabled=True, max_followers=8500)
+session.like_by_tags(["programming", "python"], amount=5)
+session.set_dont_like(["naked", "nsfw"])
+session.end()
